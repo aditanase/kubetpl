70b988548a002c76f0610b53d8d5ffda9989396c
Adrian Tanase <atanase@adobe.com>
2018-11-13 15:13:51 +0200

    fix map keys

--- kubetpl.go
+++ kubetpl.go
@@ -552,9 +552,38 @@ func readFile(path string) ([]byte, error) {
 }
 
 func parseYAML(data []byte) (map[string]interface{}, error) {
-	m := make(map[string]interface{})
+	m := make(map[interface{}]interface{})
 	yaml.Unmarshal(data, &m)
-	return m, nil
+	return cleanupMapValue(m).(map[string]interface{}), nil
+}
+
+func cleanupInterfaceArray(in []interface{}) []interface{} {
+    res := make([]interface{}, len(in))
+    for i, v := range in {
+        res[i] = cleanupMapValue(v)
+    }
+    return res
+}
+
+func cleanupInterfaceMap(in map[interface{}]interface{}) map[string]interface{} {
+    res := make(map[string]interface{})
+    for k, v := range in {
+        res[fmt.Sprintf("%v", k)] = cleanupMapValue(v)
+    }
+    return res
+}
+
+func cleanupMapValue(v interface{}) interface{} {
+    switch v := v.(type) {
+    case []interface{}:
+        return cleanupInterfaceArray(v)
+    case map[interface{}]interface{}:
+        return cleanupInterfaceMap(v)
+    case string:
+        return v
+    default:
+        return fmt.Sprintf("%v", v)
+    }
 }
 
 func parseDotEnv(data []byte) (map[string]interface{}, error) {
